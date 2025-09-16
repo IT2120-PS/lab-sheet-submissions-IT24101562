202e1fe6e1fc81c4e2044efa2c8b4fffb49ecd5b
IT24101562 <IT24101562@my.sliit.lk>
2025-09-16 13:14:45 +0530

    Lab_6

---
+++ Lab Sheet 06/IT24101562_lab6.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\ASUS TUF\\OneDrive - Sri Lanka Institute of Information Technology\\sliit\\Modules\\Y2 S1\\PS\\Lab6")
+#(1)
+#i  Binomial Distribution
+#Here, Random variable X has Binomial Distribution with n=50 and p=0.85
+
+#ii
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#(2)
+
+#i Customer Calls per hour
+
+#ii Poisson Distribution 
+# Here, Random variable X has Poisson Distribution with λ=12
+
+#iii
+dpois(15,12)
