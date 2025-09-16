6ac775d88327612a63ef090d6419277f2e31b70e
IT24101562 <IT24101562@my.sliit.lk>
2025-09-16 14:43:34 +0530

    Lab_7

---
+++ Lab Sheet 07/IT24101562_Lab7.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\ASUS TUF\\OneDrive - Sri Lanka Institute of Information Technology\\sliit\\Modules\\Y2 S1\\PS\\Lab6")
+--(1)
+
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+--(2)
+pexp(2, rate=1/3)
+
+
+--(3)
+--i 
+1 - pnorm(130, mean=100, sd=15)
+
+
+--ii
+qnorm(0.95, mean=100, sd=15)
+
